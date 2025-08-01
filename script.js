43da264087dd247d9f05839f5e94620285f5273c
surya2prakash <sv251171@gmail.com>
2025-08-01 23:48:57 +0530

    script

---
+++ script.js
@@ -0,0 +1,6 @@
+
+let start = document.getElementById("start");
+let end = document.getElementById("end");
+let restart = document.getElementById("restart");
+
+let timer = document.getElementById("timer");
